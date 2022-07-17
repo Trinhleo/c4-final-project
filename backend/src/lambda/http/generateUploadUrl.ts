216fc5aaa03bf7e608b5d0b1dbd6bae7eef471ab
Trinh Ho <trinhhnk@unifiedpost.com>
2022-07-18 03:20:36 +0700

    development: Updated code

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -6,12 +6,18 @@ import { cors, httpErrorHandler } from 'middy/middlewares';
 
 import { getUserId } from '../utils';
 import { createAttachmentPresignedUrl } from '../../helpers/todos';
+import { createLogger } from '../../utils/logger';
+
+const logger = createLogger('generateUploadUrl');
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info(`Processing event: ${{ event: event }}`);
+
     const todoId = event.pathParameters.todoId;
     const userId = getUserId(event);
 
+    logger.info(`createAttachmentPresignedUrl todo ${todoId} for user ${userId}`);
     const uploadUrl = await createAttachmentPresignedUrl(userId, todoId);
 
     return {
